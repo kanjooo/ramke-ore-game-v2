2fab6b8b928b03c05ada3ddc371f85dc707a2ce6
kanjooo <bramanauskas04@gmail.com>
2023-05-13 01:45:44 +0300

    Adding movement

---
+++ game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultipier;
+    bool readyToJump = true;
+
+    [Header("Ground Check")]
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+
+    [Header("Wall Check")]
+    public LayerMask wallLayer;
+    public float wallDistance;
+    public float wallSlideSpeed;
+ 
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(isGrounded());
+        GetInput();
+        SpeedControl();
+
+        if (isGrounded())
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }     
+    }
+
+    void FixedUpdate()
+    {
+        //Wall Slide
+        if (isTouchingWall() && !isGrounded())
+        {
+            rb.AddForce(Vector3.down * wallSlideSpeed, ForceMode.VelocityChange);
+        }
+        MovePlayer();
+    }
+
+    bool isGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, groundLayer);
+    }
+    bool isTouchingWall()
+    {
+        return Physics.Raycast(transform.position, transform.right, wallDistance, wallLayer)
+                   || Physics.Raycast(transform.position, -transform.right, wallDistance, wallLayer);
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && isGrounded())
+        {
+            Debug.Log("jump");
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        //on ground
+        if (isGrounded())
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        //in air
+        else if(!isGrounded())
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultipier, ForceMode.Force);
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x,0f,rb.velocity.z);
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x,0f,rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
